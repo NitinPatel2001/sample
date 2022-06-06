454e225f336fcd9300886e39f5e2764de8a244a3
NitinPatel2001 <nitinpatel_co20b5_40@dtu.ac.in>
2022-06-06 13:51:56 +0530

    PORT

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const res = require('express/lib/response')
 const app = express()
 
+const PORT = process.env.PORT || 4444
+
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
 
-app.listen(4003,()=>{
-    console.log("http://localhost:4003")
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`)
 })
